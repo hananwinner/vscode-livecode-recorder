95de1652c9560e73199bf5bbb45bfedd8f0e07ef
Hanan Wiener <hanan888@gmail.com>
2023-03-21 14:39:48 +0200

    reverting last jest related changes

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    // Other Jest configuration options...
-    testPathIgnorePatterns: [
-      '/node_modules/',
-      '/dist/',
-      '/*.js' // Add this line to ignore JS files
-    ]
-  };
-  
